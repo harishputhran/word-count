a86e70c90bde96b05e98227c9a35d3adbf93ad4c
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 00:10:15 +0530

    RED:Case - Unique Words and Count for Space Delimited Text (2 words)

--- src/test/java/com/clean/code/WordCountCalculatorShould.java
+++ src/test/java/com/clean/code/WordCountCalculatorShould.java
@@ -17,4 +17,13 @@ public class WordCountCalculatorShould {
 		expectedUniqueWordsAndItsCount.put(helloText, 1);
 		assertEquals(expectedUniqueWordsAndItsCount, wordCountCalculator.countUniqueWords(helloText));		
 	}
+	
+	@Test
+	public void return_map_of_Unique_Words_and_its_Count_When_Input_Word_is_Space_Delimited_Text_consisting_of_2_Words(){
+		WordCountCalculator wordCountCalculator = new WordCountCalculator();
+		Map<String, Integer> expectedUniqueWordsAndItsCount = new HashMap<>();
+		expectedUniqueWordsAndItsCount.put("Hello", 1);
+		expectedUniqueWordsAndItsCount.put("World", 1);
+		assertEquals(expectedUniqueWordsAndItsCount, wordCountCalculator.countUniqueWords("Hello World"));
+	}
 }
