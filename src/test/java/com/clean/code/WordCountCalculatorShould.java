cdaf80e0f7537ef9b2f6afd5d55dddd0e338f051
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 00:03:33 +0530

    RED : Test Case - Return Unique Word and its Count for Single Word input

--- src/test/java/com/clean/code/WordCountCalculatorShould.java
+++ src/test/java/com/clean/code/WordCountCalculatorShould.java
@@ -1,14 +1,20 @@
 package com.clean.code;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
 public class WordCountCalculatorShould {
 
 	@Test
-	public void return_One_as_Count_of_Unique_Words_When_Input_is_One_Word(){
+	public void return_map_of_Unique_Words_and_its_Count_When_Input_is_One_Word(){
 		WordCountCalculator wordCountCalculator = new WordCountCalculator();
-		assertEquals(1, wordCountCalculator.countUniqueWords("Hello"));		
+		Map<String, Integer> expectedUniqueWordsAndItsCount = new HashMap<>();
+		String helloText = "Hello";
+		expectedUniqueWordsAndItsCount.put(helloText, 1);
+		assertEquals(expectedUniqueWordsAndItsCount, wordCountCalculator.countUniqueWords(helloText));		
 	}
 }
