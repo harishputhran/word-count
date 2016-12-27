905d8d8b2566f2739b196678f582887a67929561
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 01:04:26 +0530

    GREEN: Added case for Input Delimited Text with Multiple Words

--- src/test/java/com/clean/code/WordCountCalculatorShould.java
+++ src/test/java/com/clean/code/WordCountCalculatorShould.java
@@ -14,7 +14,7 @@ public class WordCountCalculatorShould {
 	private final Long WORD_COUNT_2 = valueOf(2);
 	private final Long WORD_COUNT_1 = valueOf(1);
 	private final String HELLO_TEXT = "Hello";
-	private final String SPACE_DELIMITER = " ";
+	private final String BLANK_SPACE_DELIMITER = " ";
 	private final String COMMA_DELIMITER = ",";
 	
 	private WordCountCalculator wordCountCalculator;
@@ -29,25 +29,34 @@ public class WordCountCalculatorShould {
 	@Test
 	public void return_map_of_Unique_Words_and_its_Count_When_Input_is_One_Word(){
 		expectedUniqueWordsCount.put(HELLO_TEXT, WORD_COUNT_1);
-		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords(HELLO_TEXT, SPACE_DELIMITER));		
+		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords(HELLO_TEXT, BLANK_SPACE_DELIMITER));		
 	}
 	
 	@Test
 	public void return_map_of_Unique_Words_and_its_Count_When_Input_Word_is_Space_Delimited_Text_consisting_of_2_Words(){
 		expectedUniqueWordsCount.put(HELLO_TEXT, WORD_COUNT_1);
 		expectedUniqueWordsCount.put("World", WORD_COUNT_1);
-		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords("Hello World", SPACE_DELIMITER));
+		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords("Hello World", BLANK_SPACE_DELIMITER));
 	}
 	
 	@Test
 	public void return_map_of_Unique_Words_and_its_Count_When_Input_Word_is_Space_Delimited_Text_consisting_of_2_Identical_Words(){
 		expectedUniqueWordsCount.put(HELLO_TEXT, WORD_COUNT_2);
-		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords("Hello Hello", SPACE_DELIMITER));
+		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords("Hello Hello", BLANK_SPACE_DELIMITER));
 	}
 	
 	@Test
 	public void return_map_of_Unique_Words_and_its_Count_When_Input_Word_is_Comma_Delimited_Text_consisting_of_2_Identical_Words(){		
-		expectedUniqueWordsCount.put(HELLO_TEXT, valueOf(2));
+		expectedUniqueWordsCount.put(HELLO_TEXT, WORD_COUNT_2);
 		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords("Hello,Hello", COMMA_DELIMITER));
+	}
+	
+	@Test
+	public void return_map_of_Unique_Words_and_its_Count_When_Input_Word_is_Space_Delimited_Text_consisting_of_Multiple_Words(){		
+		expectedUniqueWordsCount.put("a", WORD_COUNT_2);
+		expectedUniqueWordsCount.put("test", WORD_COUNT_1);
+		expectedUniqueWordsCount.put("for", WORD_COUNT_1);
+		expectedUniqueWordsCount.put("word count", WORD_COUNT_1);
+		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords("a,test,for,a,word count", COMMA_DELIMITER));
 	}
 }
