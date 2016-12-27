8029d01bf19a3407d2c8af608672e1725c236ed8
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 00:06:59 +0530

    GREEN:Test to check for Unique Word and its Count instead of only Count

--- src/main/java/com/clean/code/WordCountCalculator.java
+++ src/main/java/com/clean/code/WordCountCalculator.java
@@ -1,8 +1,14 @@
 package com.clean.code;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCountCalculator {
 	
-	public int countUniqueWords(String inputWord) {		
-		return 1;
+	public Map<String, Integer> countUniqueWords(String inputWord) {
+		Map<String, Integer> wordAndCount = new HashMap<>();
+		wordAndCount.put(inputWord, 1);
+		return wordAndCount;
+
 	}
 }
