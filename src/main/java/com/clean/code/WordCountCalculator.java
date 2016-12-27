d6170f0b0be67f3e5f2967e718985f7ff2f867d5
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 00:31:10 +0530

    REFACTOR: Code and Test Cases refactoring.

--- src/main/java/com/clean/code/WordCountCalculator.java
+++ src/main/java/com/clean/code/WordCountCalculator.java
@@ -6,15 +6,12 @@ import java.util.Map;
 public class WordCountCalculator {
 	
 	public Map<String, Integer> countUniqueWords(String inputWord) {
-		Map<String, Integer> wordAndCount = new HashMap<>();
+		Map<String, Integer> wordsCount = new HashMap<>();
 		String SPACE_DELIMITER = " ";
-		if(inputWord.contains(SPACE_DELIMITER)){
-			String[] words = inputWord.split(SPACE_DELIMITER);
-			wordAndCount.put(words[0], 1);
-			wordAndCount.put(words[1], 1);
-		}else{
-			wordAndCount.put(inputWord, 1);
-		}		
-		return wordAndCount;
+		String[] words = inputWord.split(SPACE_DELIMITER);
+		for(String word : words){
+			wordsCount.put(word, 1);
+		}
+		return wordsCount;
 	}
 }
--- src/test/java/com/clean/code/WordCountCalculatorShould.java
+++ src/test/java/com/clean/code/WordCountCalculatorShould.java
@@ -5,25 +5,31 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WordCountCalculatorShould {
+	
+	private WordCountCalculator wordCountCalculator;
+	private Map<String, Integer> expectedUniqueWordsCount;
+	
+	@Before
+	public void setUp(){
+		wordCountCalculator = new WordCountCalculator();
+		expectedUniqueWordsCount = new HashMap<>();
+	}
 
 	@Test
 	public void return_map_of_Unique_Words_and_its_Count_When_Input_is_One_Word(){
-		WordCountCalculator wordCountCalculator = new WordCountCalculator();
-		Map<String, Integer> expectedUniqueWordsAndItsCount = new HashMap<>();
 		String helloText = "Hello";
-		expectedUniqueWordsAndItsCount.put(helloText, 1);
-		assertEquals(expectedUniqueWordsAndItsCount, wordCountCalculator.countUniqueWords(helloText));		
+		expectedUniqueWordsCount.put(helloText, 1);
+		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords(helloText));		
 	}
 	
 	@Test
 	public void return_map_of_Unique_Words_and_its_Count_When_Input_Word_is_Space_Delimited_Text_consisting_of_2_Words(){
-		WordCountCalculator wordCountCalculator = new WordCountCalculator();
-		Map<String, Integer> expectedUniqueWordsAndItsCount = new HashMap<>();
-		expectedUniqueWordsAndItsCount.put("Hello", 1);
-		expectedUniqueWordsAndItsCount.put("World", 1);
-		assertEquals(expectedUniqueWordsAndItsCount, wordCountCalculator.countUniqueWords("Hello World"));
+		expectedUniqueWordsCount.put("Hello", 1);
+		expectedUniqueWordsCount.put("World", 1);
+		assertEquals(expectedUniqueWordsCount, wordCountCalculator.countUniqueWords("Hello World"));
 	}
 }
