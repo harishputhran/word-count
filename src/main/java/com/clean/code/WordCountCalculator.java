5a0508acf1c9e0b300847a314ffc4236e40cabcb
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 00:42:16 +0530

    REFACTOR:Removed for loop, if-else. Used Java 8 provided features.

--- src/main/java/com/clean/code/WordCountCalculator.java
+++ src/main/java/com/clean/code/WordCountCalculator.java
@@ -1,22 +1,18 @@
 package com.clean.code;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class WordCountCalculator {
 	
-	public Map<String, Integer> countUniqueWords(String inputWord) {
-		Map<String, Integer> wordsCount = new HashMap<>();
+	public Map<String, Long> countUniqueWords(String inputWord) {
 		String SPACE_DELIMITER = " ";
 		String[] words = inputWord.split(SPACE_DELIMITER);
-		for(String word : words){
-			if(wordsCount.containsKey(word)){
-				Integer countForWord = wordsCount.get(word);
-				wordsCount.put(word, countForWord + 1);
-			}else{
-				wordsCount.put(word, 1);
-			}
-		}
-		return wordsCount;
+		return Arrays.asList(words)
+	                 .stream()
+		             .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
 	}
 }
